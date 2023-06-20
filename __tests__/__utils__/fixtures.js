51c0d3689aa8e3ccfbbb47687bca50ac48f9c24c
John Rake <john@betablox.com>
2023-06-20 18:15:30 -0500

    support fixture directories

--- __tests__/__utils__/fixtures.js
+++ __tests__/__utils__/fixtures.js
@@ -1,8 +1,14 @@
-import {copyFile} from "fs/promises";
+import { cp } from "node:fs/promises";
 
 export const useFixture = async (name) => {
     const src = `${process.env.__FIXTURES_DIR__}/${name}`
-    const dest = `${process.env.__TMP_DIR__}/${name}`
-    await copyFile(src, dest)
+
+    // We nest fixtures, but need a simple way to assert files against a their predetermined name
+    // (e.g. app.config.js or package.json) This has obvious problems with duplicate names so you cannot
+    // ever copy and assert two files of the same name in the same test. This is acceptable tech debt for now.
+    const baseFileName = name.split("/")[1]
+    const dest = `${process.env.__TMP_DIR__}/${baseFileName}`
+
+    await cp(src, dest, { recursive: true })
     return dest
 }
--- __tests__/cli.spec.js
+++ __tests__/cli.spec.js
@@ -1,6 +1,6 @@
 const createReleaseCmd = require("../commands/create-release")
-const {cleanTmpFiles} = require("./__utils__/helpers");
-const {useFixture} = require("./__utils__/fixtures");
+const { cleanTmpFiles } = require("./__utils__/helpers");
+const { useFixture } = require("./__utils__/fixtures");
 const {
     assertAppConfig,
     assertPackageJson,
@@ -17,7 +17,7 @@ process.exit = jest.fn()
 beforeEach(async () => {
     await cleanTmpFiles()
 
-    git.status = jest.fn((cb) => cb(null, {modified: []}))
+    git.status = jest.fn((cb) => cb(null, { modified: [] }))
 })
 
 afterAll(async () => {
@@ -25,146 +25,150 @@ afterAll(async () => {
     await cleanTmpFiles()
 })
 
-it("updates an app.config.js", async () => {
-    const appConfigPath = await useFixture("app.config.js")
+describe("CLI", () => {
+    describe("-r 'release channel versions'", () => {
+        it("updates an app.config.js", async () => {
+            const appConfigPath = await useFixture("release-channel-versions/app.config.js")
 
-    const args = {
-        release: "1.1.0",
-        appConfigPath,
-    }
-    await createReleaseCmd(args)
+            const args = {
+                release: "1.1.0",
+                appConfigPath,
+            }
+            await createReleaseCmd(args)
 
-    await assertAppConfig({
-        version: "1.1.0",
-        ios: {buildNumber: "2"},
-        android: {versionCode: 2}
+            await assertAppConfig({
+                version: "1.1.0",
+                ios: { buildNumber: "2" },
+                android: { versionCode: 2 }
+            })
+            await assertSuccess()
+        });
+
+        it("updates an eas.json", async () => {
+            const appConfigPath = await useFixture("release-channel-versions/app.config.js")
+            const easJsonPath = await useFixture("release-channel-versions/eas.json")
+
+            const args = {
+                release: "1.1.0",
+                easJsonPath,
+                appConfigPath,
+            }
+            await createReleaseCmd(args)
+
+            await assertEasJson({
+                build: {
+                    staging: { releaseChannel: "staging-1.1.0" },
+                    prod: { releaseChannel: "prod-1.1.0" }
+                },
+            })
+            await assertSuccess()
+        })
+
+        it("updates a package.json", async () => {
+            const appConfigPath = await useFixture("release-channel-versions/app.config.js")
+            const packageJsonPath = await useFixture("release-channel-versions/package.json")
+
+            const args = {
+                release: "1.1.0",
+                packageJsonPath,
+                appConfigPath,
+            }
+            await createReleaseCmd(args)
+
+            await assertPackageJson({ version: "1.1.0" })
+            await assertSuccess()
+        })
+
+        it("updates all files", async () => {
+            const appConfigPath = await useFixture("release-channel-versions/app.config.js")
+            const easJsonPath = await useFixture("release-channel-versions/eas.json")
+            const packageJsonPath = await useFixture("release-channel-versions/package.json")
+
+            const args = {
+                release: "1.1.0",
+                appConfigPath,
+                easJsonPath,
+                packageJsonPath,
+            }
+            await createReleaseCmd(args)
+
+            await assertAppConfig({
+                version: "1.1.0",
+                ios: { buildNumber: "2" },
+                android: { versionCode: 2 }
+            })
+            await assertEasJson({
+                build: {
+                    staging: { releaseChannel: "staging-1.1.0" },
+                    prod: { releaseChannel: "prod-1.1.0" }
+                },
+            })
+            await assertPackageJson({ version: "1.1.0" })
+            await assertSuccess()
+        })
+
+        it("throws an error for an invalid semver version", async () => {
+            const appConfigPath = await useFixture("release-channel-versions/app.config.js")
+            const easJsonPath = await useFixture("release-channel-versions/eas.json")
+            const packageJsonPath = await useFixture("release-channel-versions/package.json")
+
+            const args = {
+                release: "bad-version",
+                appConfigPath,
+                easJsonPath,
+                packageJsonPath,
+            }
+            await createReleaseCmd(args)
+
+            await assertAppConfig({
+                version: "1.0.0",
+                ios: { buildNumber: "1" },
+                android: { versionCode: 1 }
+            })
+            await assertEasJson({
+                build: {
+                    staging: { releaseChannel: "staging-1.0.0" },
+                    prod: { releaseChannel: "prod-1.0.0" }
+                },
+            })
+            await assertPackageJson({ version: "1.0.0" })
+            await assertFailed([
+                "Invalid app version: 'bad-version'. Please follow https://semver.org/"
+            ])
+        })
+
+        it("throws an error git workspace is not clean", async () => {
+            const appConfigPath = await useFixture("release-channel-versions/app.config.js")
+            const easJsonPath = await useFixture("release-channel-versions/eas.json")
+            const packageJsonPath = await useFixture("release-channel-versions/package.json")
+            const modifiedFiles = ["file.txt", "service.js", "README.md"]
+            git.status = jest.fn((cb) => cb(null, { modified: modifiedFiles }))
+
+            const args = {
+                release: "1.1.0",
+                appConfigPath,
+                easJsonPath,
+                packageJsonPath,
+            }
+            await createReleaseCmd(args)
+
+            await assertAppConfig({
+                version: "1.0.0",
+                ios: { buildNumber: "1" },
+                android: { versionCode: 1 }
+            })
+            await assertEasJson({
+                build: {
+                    staging: { releaseChannel: "staging-1.0.0" },
+                    prod: { releaseChannel: "prod-1.0.0" }
+                },
+            })
+            await assertPackageJson({ version: "1.0.0" })
+            await assertFailed([
+                "Invalid git workspace",
+                "You have git working changes. Please resolve the following files before continuing.",
+                modifiedFiles,
+            ])
+        })
     })
-    await assertSuccess()
-})
-
-it("updates an eas.json", async () => {
-    const appConfigPath = await useFixture("app.config.js")
-    const easJsonPath = await useFixture("eas.json")
-
-    const args = {
-        release: "1.1.0",
-        easJsonPath,
-        appConfigPath,
-    }
-    await createReleaseCmd(args)
-
-    await assertEasJson({
-        build: {
-            staging: {releaseChannel: "staging-1.1.0"},
-            prod: {releaseChannel: "prod-1.1.0"}
-        },
-    })
-    await assertSuccess()
-})
-
-it("updates a package.json", async () => {
-    const appConfigPath = await useFixture("app.config.js")
-    const packageJsonPath = await useFixture("package.json")
-
-    const args = {
-        release: "1.1.0",
-        packageJsonPath,
-        appConfigPath,
-    }
-    await createReleaseCmd(args)
-
-    await assertPackageJson({version: "1.1.0"})
-    await assertSuccess()
-})
-
-it("updates all files", async () => {
-    const appConfigPath = await useFixture("app.config.js")
-    const easJsonPath = await useFixture("eas.json")
-    const packageJsonPath = await useFixture("package.json")
-
-    const args = {
-        release: "1.1.0",
-        appConfigPath,
-        easJsonPath,
-        packageJsonPath,
-    }
-    await createReleaseCmd(args)
-
-    await assertAppConfig({
-        version: "1.1.0",
-        ios: {buildNumber: "2"},
-        android: {versionCode: 2}
-    })
-    await assertEasJson({
-        build: {
-            staging: {releaseChannel: "staging-1.1.0"},
-            prod: {releaseChannel: "prod-1.1.0"}
-        },
-    })
-    await assertPackageJson({version: "1.1.0"})
-    await assertSuccess()
-})
-
-it("throws an error for an invalid semver version", async () => {
-    const appConfigPath = await useFixture("app.config.js")
-    const easJsonPath = await useFixture("eas.json")
-    const packageJsonPath = await useFixture("package.json")
-
-    const args = {
-        release: "bad-version",
-        appConfigPath,
-        easJsonPath,
-        packageJsonPath,
-    }
-    await createReleaseCmd(args)
-
-    await assertAppConfig({
-        version: "1.0.0",
-        ios: {buildNumber: "1"},
-        android: {versionCode: 1}
-    })
-    await assertEasJson({
-        build: {
-            staging: {releaseChannel: "staging-1.0.0"},
-            prod: {releaseChannel: "prod-1.0.0"}
-        },
-    })
-    await assertPackageJson({version: "1.0.0"})
-    await assertFailed([
-        "Invalid app version: 'bad-version'. Please follow https://semver.org/"
-    ])
-})
-
-it("throws an error git workspace is not clean", async () => {
-    const appConfigPath = await useFixture("app.config.js")
-    const easJsonPath = await useFixture("eas.json")
-    const packageJsonPath = await useFixture("package.json")
-    const modifiedFiles = ["file.txt", "service.js", "README.md"]
-    git.status = jest.fn((cb) => cb(null, {modified: modifiedFiles}))
-
-    const args = {
-        release: "1.1.0",
-        appConfigPath,
-        easJsonPath,
-        packageJsonPath,
-    }
-    await createReleaseCmd(args)
-
-    await assertAppConfig({
-        version: "1.0.0",
-        ios: {buildNumber: "1"},
-        android: {versionCode: 1}
-    })
-    await assertEasJson({
-        build: {
-            staging: {releaseChannel: "staging-1.0.0"},
-            prod: {releaseChannel: "prod-1.0.0"}
-        },
-    })
-    await assertPackageJson({version: "1.0.0"})
-    await assertFailed([
-        "Invalid git workspace",
-        "You have git working changes. Please resolve the following files before continuing.",
-        modifiedFiles,
-    ])
 })
